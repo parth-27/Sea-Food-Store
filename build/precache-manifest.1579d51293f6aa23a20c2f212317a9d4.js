4875a0875ea4bfba5e548eb6d090419179b98dcd
parth-27 <parth1020patel@gmail.com>
2020-07-26 21:09:30 +0530

    done

---
+++ build/precache-manifest.1579d51293f6aa23a20c2f212317a9d4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d483035dcc0f23bb1a879cf2c94044c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc8254ad907d36994174",
+    "url": "/static/css/main.7eea5820.chunk.css"
+  },
+  {
+    "revision": "05ba33d2ea0ac1a406ff",
+    "url": "/static/js/2.b2e13e4f.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.b2e13e4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8254ad907d36994174",
+    "url": "/static/js/main.2fd8375e.chunk.js"
+  },
+  {
+    "revision": "9c9483d5b158c3bdd84b",
+    "url": "/static/js/runtime-main.a3df29e7.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
